93be893ae8566c9ef68722d72a9ac5f20f3c713d
JYCuzz <hanafos944@naver.com>
2025-06-05 17:36:47 +0900

    1

---
+++ src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoreuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreUserApiApplication.class, args);
+	}
+
+}

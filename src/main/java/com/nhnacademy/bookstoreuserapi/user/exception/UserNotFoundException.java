a0c2fd242a098f517498cab54a03c1a08a8ae3ab
JYCuzz <hanafos944@naver.com>
2025-07-02 10:46:34 +0900

    feat : sonarqube 관련 이슈 해결

--- src/main/java/com/nhnacademy/bookstoreuserapi/user/exception/UserNotFoundException.java
+++ src/main/java/com/nhnacademy/bookstoreuserapi/user/exception/UserNotFoundException.java
@@ -3,14 +3,18 @@ package com.nhnacademy.bookstoreuserapi.user.exception;
 import com.nhnacademy.bookstoreuserapi.common.exception.NotFoundException;
 
 public class UserNotFoundException extends NotFoundException {
+
+    private static final String NOT_FOUND = " not found";
+
     public UserNotFoundException(String userId) {
-        super("User ID : " + userId + " not found");
+        super("User ID : " + userId + NOT_FOUND);
     }
     public UserNotFoundException(Long userNo) {
-        super("User No : " + userNo + " not found");
+        super("User No : " + userNo + NOT_FOUND);
     }
     public UserNotFoundException(String userName, String userEmail) {
-        super("User Name : " + userName + " Email : " + userEmail + " not found");
+        super("User Name : " + userName + " Email : " + userEmail + NOT_FOUND);
     }
-
 }
+
+

15d4bef1690b974bf9b360af0792ffef2cb677b8
JYCuzz <hanafos944@naver.com>
2025-07-07 16:03:55 +0900

    task : datasource 관련 로그 출력 함수 리팩토링

--- src/main/java/com/nhnacademy/bookstoreuserapi/common/config/DataSourceChecker.java
+++ src/main/java/com/nhnacademy/bookstoreuserapi/common/config/DataSourceChecker.java
@@ -1,20 +1,20 @@
 package com.nhnacademy.bookstoreuserapi.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 @Component
+@RequiredArgsConstructor
 public class DataSourceChecker implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(DataSourceChecker.class);
 
-    @Autowired
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     @Override
     public void run(String... args) {

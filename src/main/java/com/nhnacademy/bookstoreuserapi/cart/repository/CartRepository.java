0acc9a094a1fd5f20cb2388071dfa51eeec74a1b
NaIlHwan <nlhjobs01@naver.com>
2025-07-09 11:17:23 +0900

    feat: 고아 장바구니 자동 정리 스케줄링 추가

--- src/main/java/com/nhnacademy/bookstoreuserapi/cart/repository/CartRepository.java
+++ src/main/java/com/nhnacademy/bookstoreuserapi/cart/repository/CartRepository.java
@@ -1,12 +1,17 @@
 package com.nhnacademy.bookstoreuserapi.cart.repository;
 
 import com.nhnacademy.bookstoreuserapi.cart.domain.Cart;
+import com.nhnacademy.bookstoreuserapi.cart.domain.OwnerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     boolean existsByUser_UserId(String userId);
     Optional<Cart> findByUser_UserId(String userId);
     Optional<Cart> findByGuestUUID(String guestUUID);
+
+    void deleteByOwnerTypeAndCreatedAtBefore(OwnerType ownerType, LocalDateTime dateTime);
+    void deleteByOwnerTypeAndUpdatedAtBefore(OwnerType ownerType, LocalDateTime dateTime);
 }

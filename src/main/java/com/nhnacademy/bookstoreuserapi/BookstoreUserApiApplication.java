d9088e677fc0f730ad7258aa79e6a2561525cc85
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-14 13:59:39 +0900

    feat(payment): LoadBalanced 사용해 user-api서비스 이름으로 호출(회원 정보 가져오기 위해 작성)

--- src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
+++ src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
@@ -3,8 +3,11 @@ package com.nhnacademy.bookstoreuserapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,4 +19,9 @@ public class BookstoreUserApiApplication {
 		SpringApplication.run(BookstoreUserApiApplication.class, args);
 	}
 
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }

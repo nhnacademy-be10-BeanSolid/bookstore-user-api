113d9be46a4fb2a3d5a07aeb93e3f19f4c392a8d
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-14 21:30:08 +0900

    feat: RabbitMQ properties 추가했으므로 restTemplate 삭제

--- src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
+++ src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
@@ -3,25 +3,14 @@ package com.nhnacademy.bookstoreuserapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @EnableScheduling
 public class BookstoreUserApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreUserApiApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 }

c1a3f04086c1c2a8f6f7b1b02e9574c70ae33c74
gkwjdtn975 <gkwjdtn975@naver.com>
2025-07-15 15:49:04 +0900

    feat: 유저가 가입됐을 때 이벤트처리를 하기위한 추가 - publisher(유저): 회원가입 시 rabbitTemplate.converAndSend()로 메시지 가능

--- src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
+++ src/main/java/com/nhnacademy/bookstoreuserapi/BookstoreUserApiApplication.java
@@ -1,16 +1,36 @@
 package com.nhnacademy.bookstoreuserapi;
 
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableScheduling
 public class BookstoreUserApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreUserApiApplication.class, args);
+	}
+
+	@Bean
+	public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jackson2JsonMessageConverter) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter);
+		return rabbitTemplate;
 	}
 }
